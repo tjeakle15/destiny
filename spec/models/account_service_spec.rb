4e8e2b5cc8b3d14818801883f305f927c9f76f31
TJ Eakle <teakle@covermymeds.com>
2016-01-28 09:46:16 -0600

    Fixed tests

--- spec/models/account_service_spec.rb
+++ spec/models/account_service_spec.rb
@@ -1,26 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AccountService, type: :model do
-  VALID_XBOX_PLAYER = "tj71587"
-  INVALID_MEMBERSHIP_ID = "11111111111111111111111111111111"
-  VALID_XBOX_PLAYER_CHARACTER_NUMBER = 3
-  INVALID_MEMBERSHIP_ID_ERROR_CODE = 7
 
   it "finds an account with a valid membership id" do
-    player_service = PlayerService.find("tj71587")
+    player_service = PlayerService.find(TestConstants::VALID_XBOX_GAMERTAG)
     account_service = AccountService.find(player_service.data_object.membership_id)
-    expect(account_service.Response.data.characters.size.eql?(VALID_XBOX_PLAYER_CHARACTER_NUMBER))
+    expect(account_service.Response.data.characters.size.eql?(TestConstants::VALID_XBOX_GAMERTAG_CHARACTER_NUMBER))
   end
 
   it "does not find an account with an invalid membership id" do
-    account_service = AccountService.find(INVALID_MEMBERSHIP_ID)
-    expect(account_service.ErrorCode.eql?(INVALID_MEMBERSHIP_ID_ERROR_CODE))
+    account_service = AccountService.find(TestConstants::INVALID_MEMBERSHIP_ID)
+    expect(account_service.ErrorCode.eql?(TestConstants::INVALID_MEMBERSHIP_ID_ERROR_CODE))
   end
 
   it "returns the data object with valid membership id" do
     player_service = PlayerService.find("tj71587")
     account_service = AccountService.find(player_service.data_object.membership_id)
-    expect(account_service.data_object.characters.size.eql?(VALID_XBOX_PLAYER_CHARACTER_NUMBER))
-    byebug
+    expect(account_service.data_object.characters.size.eql?(TestConstants::VALID_XBOX_GAMERTAG_CHARACTER_NUMBER))
   end
 end
--- spec/models/player_service_spec.rb
+++ spec/models/player_service_spec.rb
@@ -1,40 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe PlayerService, type: :model do
-  #Constants used in tests
-  VALID_XBOX_PLAYER = "tj71587"
-  INVALID_XBOX_PLAYER = "tj717171717"
-  INVALID_MEMBERSHIP_TYPE = "100"
-  INVALID_PLAYER_ERROR_CODE = 1
-  INVALID_MEMBERSHIP_TYPE_ERROR_CODE = 7
-
+  
   it "returns valid xbox player when xbox is specified and valid player provided" do
-    player_service = PlayerService.find(VALID_XBOX_PLAYER, MembershipType::XBOX)
-    expect(player_service.Response.first.displayName.eql?(VALID_XBOX_PLAYER))
+    player_service = PlayerService.find(TestConstants::VALID_XBOX_GAMERTAG, MembershipType::XBOX)
+    expect(player_service.Response.first.displayName.eql?(TestConstants::VALID_XBOX_GAMERTAG))
   end
 
   it "returns valid xbox player when valid player provided and no MembershipType" do
-    player_service = PlayerService.find(VALID_XBOX_PLAYER)
-    expect(player_service.Response.first.displayName.eql?(VALID_XBOX_PLAYER))
+    player_service = PlayerService.find(TestConstants::VALID_XBOX_GAMERTAG)
+    expect(player_service.Response.first.displayName.eql?(TestConstants::VALID_XBOX_GAMERTAG))
   end
 
   xit "returns valid playstation player when playstation is specified and valid player provided"
 
   it "returns nothing when invalid gamertag is specified for xbox" do
-    player_service = PlayerService.find(INVALID_XBOX_PLAYER, MembershipType::XBOX)
-    expect(player_service.ErrorCode.eql?(INVALID_PLAYER_ERROR_CODE))
+    player_service = PlayerService.find(TestConstants::INVALID_XBOX_GAMERTAG, MembershipType::XBOX)
+    expect(player_service.ErrorCode.eql?(TestConstants::INVALID_GAMERTAG_ERROR_CODE))
   end
 
   xit "returns nothing when invalid gamertag is speficifed for playstaion"
 
   it "returns nothing when invalid platform is specified" do
-    player_service = PlayerService.find(VALID_XBOX_PLAYER, INVALID_MEMBERSHIP_TYPE)
-    expect(player_service.ErrorCode.eql?(INVALID_MEMBERSHIP_TYPE_ERROR_CODE))
+    player_service = PlayerService.find(TestConstants::VALID_XBOX_GAMERTAG, TestConstants::INVALID_MEMBERSHIP_TYPE)
+    expect(player_service.ErrorCode.eql?(TestConstants::INVALID_MEMBERSHIP_TYPE_ERROR_CODE))
   end
 
   it "popluates and returns valid data object" do
-    player_service = PlayerService.find(VALID_XBOX_PLAYER, MembershipType::XBOX)
-    expect(player_service.data_object.display_name.eql?(VALID_XBOX_PLAYER))
+    player_service = PlayerService.find(TestConstants::VALID_XBOX_GAMERTAG, MembershipType::XBOX)
+    expect(player_service.data_object.display_name.eql?(TestConstants::VALID_XBOX_GAMERTAG))
   end
 
 end
